680ec54d7cb3b745465c1d3f910906aca5886766
Andrei Baloleanu <andrei@git-tower.com>
2025-12-12 14:00:29 +0100

    Add file writing functionality

--- file_ops.py
+++ file_ops.py
@@ -5,3 +5,8 @@ def read_file(file_path):
     """Read contents from a file."""
     with open(file_path, 'r') as f:
         return f.read()
+
+def write_file(file_path, content):
+    """Write contents to a file."""
+    with open(file_path, 'w') as f:
+        f.write(content)
